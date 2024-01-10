d0e3bfe3c4c8edfca2be167df72f661298228902
phamngochaigr <phamngochaimmo@gmail.com>
2024-01-11 00:52:49 +0700

    innit ipconfig

--- app/src/main/java/com/example/macmavenapp/Retrofit/RetrofitService.java
+++ app/src/main/java/com/example/macmavenapp/Retrofit/RetrofitService.java
@@ -9,7 +9,7 @@ import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitService {
-    private String IPAddress="192.168.2.181"; //cafe
+    private String IPAddress="192.168.38.72";
     OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
             .connectTimeout(40, TimeUnit.SECONDS)
             .readTimeout(60, TimeUnit.SECONDS)
